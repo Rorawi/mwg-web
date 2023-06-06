e461630210f0976e2c563ebe5d3875581f10d291
Winifred <winifredasante15@gmail.com>
2023-06-06 14:55:07 -0400

    added pagination

--- src/components/SwiperComp.js
+++ src/components/SwiperComp.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import React, { useState, useRef, useEffect } from "react";
+import { Navigation, Scrollbar, A11y } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import img from "../assets/mwg.hub.png";
 
@@ -7,14 +7,15 @@ import styles from "../components/swiper.module.css";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
-import "swiper/css/pagination";
+// import "swiper/css/pagination";
 import "swiper/css/scrollbar";
+import SwiperCore, { Pagination } from "swiper";
+
 import { Link } from "react-router-dom";
 import { BsFillPersonFill, BsClockFill } from "react-icons/bs";
 import { AiOutlineArrowRight } from "react-icons/ai";
 
 export const SwiperComp = () => {
-
   const responsiveOptions = {
     320: {
       slidesPerView: 1,
@@ -118,9 +119,19 @@ export const SwiperComp = () => {
 };
 
 export const BlogSwiper = () => {
-  const[length,setLength] = useState(false)
-  const maxContent = 100
-  const text = "Media literacy skills have become increasingly essential in today’s digital age, where young people are constantly bombarded with information from various media sources. In Ghana, Mobile Web Ghana is actively involved in promoting media literacy among youth through our youth media literacy project. This initiative aims to equip young people with the necessary skills to critically analyze and navigate the media landscape, empowering them to become informed and responsible media consumers."
+  SwiperCore.use([Pagination]);
+  const [length, setLength] = useState(false);
+  const maxContent = 100;
+  const swiperRef = useRef(null);
+
+  const handlePaginationClick = (index) => {
+    if (swiperRef.current && swiperRef.current.swiper) {
+      swiperRef.current.swiper.slideTo(index);
+    }
+  };
+
+  const text =
+    "Media literacy skills have become increasingly essential in today’s digital age, where young people are constantly bombarded with information from various media sources. In Ghana, Mobile Web Ghana is actively involved in promoting media literacy among youth through our youth media literacy project. This initiative aims to equip young people with the necessary skills to critically analyze and navigate the media landscape, empowering them to become informed and responsible media consumers.";
   const responsiveOptions = {
     1024: {
       slidesPerView: 3,
@@ -137,19 +148,43 @@ export const BlogSwiper = () => {
     <div>
       <Swiper
         // install Swiper modules
+        ref={swiperRef}
         modules={[Navigation, Pagination, Scrollbar, A11y]}
         spaceBetween={20}
         slidesPerView={3}
-        // navigation
-        breakpoints={responsiveOptions}
-        pagination={{ clickable: true }}
-        scrollbar={{ draggable: true, hide: true }}
-        onSwiper={(swiper) => console.log(swiper)}
-        effect="fade"
         loop={true}
-        onSlideChange={() => console.log("slide change")}
+        breakpoints={responsiveOptions}
+        pagination={{ clickable: false }}
+        effect="fade"
         className={styles.swiper}
       >
+        <SwiperSlide className={styles.blog_card_box}>
+          <div className={styles.img_div}>
+            <img src={img} alt="our image" />
+          </div>
+
+          <div className={styles.text_box}>
+            <div className={styles.name_time}>
+              <div className={styles.name}>
+                <BsFillPersonFill />
+                <h5>Mr.Prince</h5>
+              </div>
+
+              <div className={styles.time}>
+                <BsClockFill />
+                <h5>May 29,2023</h5>
+              </div>
+            </div>
+
+            <h2>Teaching Media Literacy</h2>
+            <p>{length ? text : `${text.slice(0, maxContent)}...`}</p>
+            <a href="#">
+              Read More
+              <AiOutlineArrowRight />
+            </a>
+          </div>
+        </SwiperSlide>
+
         <SwiperSlide className={styles.blog_card_box}>
           <div className={styles.img_div}>
             <img src={img} alt="our image" />
@@ -169,15 +204,159 @@ export const BlogSwiper = () => {
             </div>
 
             <h2>Teaching Media Literacy</h2>
-            <p>
-              {length?text:`${text.slice(0, maxContent)}...`}
-            </p>
-            <a href="#">Read More
-            <AiOutlineArrowRight/>
+            <p>{length ? text : `${text.slice(0, maxContent)}...`}</p>
+            <a href="#">
+              Read More
+              <AiOutlineArrowRight />
             </a>
           </div>
         </SwiperSlide>
 
+        <SwiperSlide className={styles.blog_card_box}>
+          <div className={styles.img_div}>
+            <img src={img} alt="our image" />
+          </div>
+
+          <div className={styles.text_box}>
+            <div className={styles.name_time}>
+              <div className={styles.name}>
+                <BsFillPersonFill />
+                <h5>Issac</h5>
+              </div>
+
+              <div className={styles.time}>
+                <BsClockFill />
+                <h5>May 29,2023</h5>
+              </div>
+            </div>
+
+            <h2>Teaching Media Literacy</h2>
+            <p>{length ? text : `${text.slice(0, maxContent)}...`}</p>
+            <a href="#">
+              Read More
+              <AiOutlineArrowRight />
+            </a>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide className={styles.blog_card_box}>
+          <div className={styles.img_div}>
+            <img src={img} alt="our image" />
+          </div>
+
+          <div className={styles.text_box}>
+            <div className={styles.name_time}>
+              <div className={styles.name}>
+                <BsFillPersonFill />
+                <h5>Winifred</h5>
+              </div>
+
+              <div className={styles.time}>
+                <BsClockFill />
+                <h5>May 29,2023</h5>
+              </div>
+            </div>
+
+            <h2>Teaching Media Literacy</h2>
+            <p>{length ? text : `${text.slice(0, maxContent)}...`}</p>
+            <a href="#">
+              Read More
+              <AiOutlineArrowRight />
+            </a>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide className={styles.blog_card_box}>
+          <div className={styles.img_div}>
+            <img src={img} alt="our image" />
+          </div>
+
+          <div className={styles.text_box}>
+            <div className={styles.name_time}>
+              <div className={styles.name}>
+                <BsFillPersonFill />
+                <h5>Miss Florence</h5>
+              </div>
+
+              <div className={styles.time}>
+                <BsClockFill />
+                <h5>May 29,2023</h5>
+              </div>
+            </div>
+
+            <h2>Teaching Media Literacy</h2>
+            <p>{length ? text : `${text.slice(0, maxContent)}...`}</p>
+            <a href="#">
+              Read More
+              <AiOutlineArrowRight />
+            </a>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className={styles.blog_card_box}>
+          <div className={styles.img_div}>
+            <img src={img} alt="our image" />
+          </div>
+
+          <div className={styles.text_box}>
+            <div className={styles.name_time}>
+              <div className={styles.name}>
+                <BsFillPersonFill />
+                <h5>Estella</h5>
+              </div>
+
+              <div className={styles.time}>
+                <BsClockFill />
+                <h5>May 29,2023</h5>
+              </div>
+            </div>
+
+            <h2>Teaching Media Literacy</h2>
+            <p>{length ? text : `${text.slice(0, maxContent)}...`}</p>
+            <a href="#">
+              Read More
+              <AiOutlineArrowRight />
+            </a>
+          </div>
+        </SwiperSlide>
+
+        <div className={styles.pagination}>
+          <button
+            className={styles.paginationButton}
+            onClick={() => handlePaginationClick(0)}
+          ></button>
+          <button
+            className={styles.paginationButton}
+            onClick={() => handlePaginationClick(1)}
+          ></button>
+          <button
+            className={styles.paginationButton}
+            onClick={() => handlePaginationClick(2)}
+          ></button>
+          <button
+            className={styles.paginationButton}
+            onClick={() => handlePaginationClick(4)}
+          ></button>
+          <button
+            className={styles.paginationButton}
+            onClick={() => handlePaginationClick(6)}
+          ></button>
+          <button
+            className={styles.paginationButton}
+            onClick={() => handlePaginationClick(8)}
+          ></button>
+          <button
+            className={styles.paginationButton}
+            onClick={() => handlePaginationClick(10)}
+          ></button>
+          <button
+            className={styles.paginationButton}
+            onClick={() => handlePaginationClick(12)}
+          ></button>
+          <button
+            className={styles.paginationButton}
+            onClick={() => handlePaginationClick(0)}
+          ></button>
+        </div>
         {/* edit swiper */}
       </Swiper>
     </div>
