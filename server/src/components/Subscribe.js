b1ce6646a5dc89b28d17a6b8e3833fd76641307b
Winifred <winifredasante15@gmail.com>
2023-06-22 12:16:42 -0400

    installed nodemon and express

--- server/src/components/Subscribe.js
+++ server/src/components/Subscribe.js
@@ -1,55 +1,86 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import styles from "../components/subscribe.module.css";
-
-
+import { CiMail } from "react-icons/ci";
 
 const Subscribe = () => {
-  const [email, setEmail] = useState('');
-  const [successMessage, setSuccessMessage] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const [name, setName] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      const response = await fetch('your-api-endpoint', {
-        method: 'POST',
+      const response = await fetch("your-api-endpoint", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ email }),
       });
 
       if (response.ok) {
-        setSuccessMessage('Thank you for subscribing!');
-        setEmail('');
+        setSuccessMessage("Thank you for subscribing!");
+        setEmail("");
       } else {
-        setErrorMessage('An error occurred. Please try again later.');
+        setErrorMessage("An error occurred. Please try again later.");
       }
     } catch (error) {
       console.error(error);
-      setErrorMessage('An error occurred. Please try again later.');
+      setErrorMessage("An error occurred. Please try again later.");
     }
   };
 
   return (
-    <div>
+    <div className={styles.container}>
       {successMessage && <p>{successMessage}</p>}
       {errorMessage && <p>{errorMessage}</p>}
 
-      <h2 className={styles.subscribe}>SUBSCRIBE TO NEWS CHANNEL</h2>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <button type="submit">Subscribe</button>
-      </form>
+      <div className={styles.subscribe_container}>
+        <div className={styles.logo_div}>
+          <CiMail />
+        </div>
+        <h2 className={styles.subscribe}>GET UPDATES FROM MOBILE WEB GHANA</h2>
+        <form onSubmit={handleSubmit}>
+          <div className={styles.twoInone}>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={name}
+              placeholder="Your Name..."
+              onChange={(e) => setName(e.target.value)}
+              required
+              
+            />
+
+            <input
+              type="text"
+              id="lastname"
+              name="lastname"
+              value={lastname}
+              placeholder="Your Lastname..."
+              onChange={(e) => setLastname(e.target.value)}
+              required
+            />
+          </div>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            placeholder="Your Email..."
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+
+          <button type="submit" id={styles.subscribe_button}>
+            Subscribe
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
